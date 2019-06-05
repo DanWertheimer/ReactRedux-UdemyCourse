1ae38769cebec428326002dceaf0b17c30265a70
Daniel Wertheimer <danwertheimer@gmail.com>
2019-06-05 16:39:47 +0200

    Completed seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,37 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-
+import Loader from "./Loader";
 class App extends React.Component {
-  constructor(props) {
-    super(props); // need to call the super function with props
+  state = { lat: null, errorMessage: "" };
 
-    // THIS IS THE ONLY TIME WE DO DIRECT
-    // ASSIGNMENT TO this.state
-    this.state = { lat: null, errorMessage: "" }; // default numeic state to null
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // We called setState!!
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
-  // MUST DEFINE THE RENDER METHOD
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading</div>;
+    return <Loader message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
