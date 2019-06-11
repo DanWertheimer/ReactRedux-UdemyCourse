6e75a8983a0bffecfb5b3ce5624a866ec400ecd2
Daniel Wertheimer <danwertheimer@gmail.com>
2019-06-11 15:57:23 +0200

    Created VideoItem & VideoList

--- videos/src/api/youtube.js
+++ videos/src/api/youtube.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const KEY = "AIzaSyBgkb_CUGJ0K0Ny_tQBjF7k3eAcGQNI6qI";
+const KEY = "AIzaSyBgkb_CUGJ0K0Ny_tQBjF7k3eAcGQNI6qI"; //THIS IS PUBLIC
 
 export default Axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
